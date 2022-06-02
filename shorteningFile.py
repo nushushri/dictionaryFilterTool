d96decc6174fff8e21dacf8e6c84fd5fe29a6c52
Anoushka <smartnushu@gmail.com>
2022-06-02 13:14:14 -0700

    deleting unnecessary files, renaming combined

--- shorteningFile.py
+++
@@ -1,9 +0,0 @@
-longList = open('words_alpha.txt', 'r')
-shortList = open('fiveLetterOnly.txt', 'w')
-
-for word in longList:
-    if(len(word) == 6):
-        shortList.write(word)
-
-longList.close()
-shortList.close()
